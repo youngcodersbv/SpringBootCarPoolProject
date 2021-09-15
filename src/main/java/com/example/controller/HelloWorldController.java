ce03b13f247faf2078e38ae6ecdb1343e7539e09
JWA van Golverdinge Schut <jaron.van.golverdinge-schut@diymaxeda.com>
2021-09-15 22:39:57 +0200

    Added controller and template

---
+++ src/main/java/com/example/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+}
