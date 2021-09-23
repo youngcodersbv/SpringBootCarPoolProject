23d1e4dd02e989dd8b9fb0f9de1e88965ac5e5ea
JWA van Golverdinge Schut <jaron.van.golverdinge-schut@diymaxeda.com>
2021-09-23 13:28:53 +0200

    Added some more test and assertions

--- src/test/java/com/example/springboot/controller/HelloWorldControllerTests.java
+++ src/test/java/com/example/springboot/controller/HelloWorldControllerTests.java
@@ -3,9 +3,9 @@ package com.example.springboot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class HelloWorldControllerTests {
@@ -15,8 +15,20 @@ public class HelloWorldControllerTests {
 
     @Test
     public void initializtionTest() {
+        // It is not null
         assertThat(controller).isNotNull();
     }
 
+    @Test
+    public void testReturnCorrectIndexTemplate() {
+        String template = controller.index();
+        assertThat(template).isEqualTo("index");
+    }
+
+    @Test
+    public void testReturnCorrectOtherTemplate() {
+        String template = controller.other();
+        assertThat(template).isEqualTo("other");
+    }
 
 }
