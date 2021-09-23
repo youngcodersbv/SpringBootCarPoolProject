09d5598b2744d06f34d210c5eb359d88bfae490b
JWA van Golverdinge Schut <jaron.van.golverdinge-schut@diymaxeda.com>
2021-09-23 04:33:06 +0200

    Logging, multiple paths and app properties

--- src/main/java/com/example/springboot/controller/HelloWorldController.java
+++ src/main/java/com/example/springboot/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.example.controller;
+package com.example.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +9,11 @@ public class HelloWorldController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/other")
+    public String other() {
+        return "other";
     }
 
 }
