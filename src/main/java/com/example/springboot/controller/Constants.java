d35e24c5b02c5f5a63a3b441fcdf8de0b45fc66a
JWA van Golverdinge Schut <jaron.van.golverdinge-schut@diymaxeda.com>
2021-10-04 10:14:59 +0200

    Usings constants here too

--- src/main/java/com/example/springboot/controller/Constants.java
+++ src/main/java/com/example/springboot/controller/Constants.java
@@ -3,4 +3,5 @@ package com.example.springboot.controller;
 public class Constants {
     public static final String REDIRECT = "redirect:";
     public static final String USERS = "/users";
+    public static final String COMMUTES = "/commutes";
 }
